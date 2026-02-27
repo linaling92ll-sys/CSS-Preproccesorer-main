8c07bf2636360ab78c0cb52b0457bb1af917f0ae
linaling92ll-sys <linaling92.ll@gmail.com>
2026-02-27 08:28:29 +0100

    Har skapat html-innehållet där kartan kommer att visas

--- src/diagram.js
+++ src/diagram.js
@@ -7,11 +7,11 @@ import Chart from 'chart.js/auto';
  * @returns {Promise <array>} JSON-data
  */
 async function fetchData() {
-    const response = await fetch("https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json");
-        if (!response.ok) {
-            throw new Error("Kunde inte hämta data");
-        }
-        return await response.json();
+  const response = await fetch("https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json");
+  if (!response.ok) {
+    throw new Error("Kunde inte hämta data");
+  }
+  return await response.json();
 }
 
 /**
@@ -19,22 +19,22 @@ async function fetchData() {
  * @param {array} courses - Lista med kurser
  */
 function createBarChart(courses) {
-    const ctx =document.getElementById('barChart');
+  const ctx = document.getElementById('barChart');
 
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: courses.map(c => c.name.split(" ")),
-            datasets: [{
-                label: 'Totalt antal sökande',
-                data: courses.map(c => Number(c.applicantsTotal.trim())),
-                backgroundColor: '#3a6ea5'
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: courses.map(c => c.name.split(" ")),
+      datasets: [{
+        label: 'Totalt antal sökande',
+        data: courses.map(c => Number(c.applicantsTotal.trim())),
+        backgroundColor: '#3a6ea5'
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
 }
 /**
  * Skapar cirkeldiagram för program
